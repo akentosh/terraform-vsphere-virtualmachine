986baedfe97dd91097d26b2d8fbc0de62c394aeb
Adam Kentosh <akentosh@gmail.com>
2020-07-01 22:14:03 -0400

    initial commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "name" {}
+variable "dc" {}
+variable "network_name" {}
+variable "cluster_name" {}
+variable "datastore_name" {}
+variable "template" {}
+variable "cpu" {}
+variable "mem" {}
+variable "ipv4_addr" {}
+variable "ipv4_mask" {}
+variable "ipv4_gw" {}
+variable "tags" {}
